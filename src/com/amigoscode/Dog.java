610b9fda20c11956ac33eeaa1f4adee955e8a02f
sostene12 <ngarukiyimanasostene@gmail.com>
2024-07-19 22:40:57 +0200

    abstract

---
+++ src/com/amigoscode/Dog.java
@@ -0,0 +1,12 @@
+package com.amigoscode;
+
+public class Dog extends Animal {
+    public Dog(String name) {
+        super(name);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Woof Woof");
+    }
+}
--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -1,64 +1,15 @@
 package com.amigoscode;
 
-import java.io.*;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args)  {
-//        START FROM LESSON 131
-//        Working with Files
-    File file = createFile("src/foo.txt");
-    writeToFile(file,true);
+//        START FROM LESSON 187
+       Animal dog = new Dog("Bobby");
+       Animal cat = new Cat("Bob");
 
-
-        try {
-            Scanner scanner = new Scanner(file);
-            while (scanner.hasNext()){
-                System.out.println(scanner.nextLine());
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
-        }
-
+//       dog.makeSound();
+//       cat.makeSound();
 
     }
-
-    private static void writeToFile(File file,boolean append){
-        //TRY WITH RESOURCES
-        try(
-                FileWriter fileWriter = new FileWriter(file,append);
-                PrintWriter writer = new PrintWriter(fileWriter);
-                ) {
-            writer.println("Jamila");
-        }
-         catch (IOException e){
-             System.out.println(e.getMessage());
-         }
-//        try {
-//            FileWriter fileWriter = new FileWriter(file,append);
-//            PrintWriter writer = new PrintWriter(fileWriter);
-//            writer.println("Come");
-//            writer.flush();
-//            writer.close();
-//        } catch (IOException e) {
-//            System.out.println(e.getMessage());
-//        }
-    }
-
-    private static File  createFile(String path){
-        try {
-            File file = new File(path);
-            if(!file.exists()){
-                file.createNewFile();
-            }
-            return  file;
-        }
-        catch (IOException e) {
-            System.out.println(e.getMessage());
-            throw new IllegalStateException(e);
-        }
-    }
-
 
 }
