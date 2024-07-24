fe3e722fd193ab5c0c708812d4a1dd8f77a00df1
sostene12 <ngarukiyimanasostene@gmail.com>
2024-07-24 09:09:46 +0200

    learning lists

---
+++ src/com/amigoscode/WorkingWithLists.java
@@ -0,0 +1,27 @@
+package com.amigoscode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WorkingWithLists {
+    public static void main(String[] args) {
+        List<String > colors = new ArrayList<>();
+        colors.add("blue");
+        colors.add("red");
+        colors.add("green");
+        System.out.println(colors.size());
+        System.out.println(colors.contains("green"));
+        System.out.println(colors.contains("pink"));
+        System.out.println(colors);
+
+        for(String color:colors){
+            System.out.println(color);
+        }
+
+        colors.forEach(System.out::println);
+
+        for (int i = 0; i < colors.size(); i++) {
+            System.out.println(colors.get(i));
+        }
+    }
+}
