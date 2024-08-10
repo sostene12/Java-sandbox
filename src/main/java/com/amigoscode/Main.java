002d44817d9cba75096bb123a8ca4429885ce9ed
sostene12 <ngarukiyimanasostene@gmail.com>
2024-08-10 22:09:32 +0200

    running maven

---
+++ src/main/java/com/amigoscode/Main.java
@@ -0,0 +1,15 @@
+package com.amigoscode;
+
+import com.github.javafaker.Faker;
+
+public class Main {
+
+    public static void main(String[] args)  {
+//        START FROM LESSON 264
+        Faker faker = new Faker();
+        String name = faker.name().fullName();
+        String email = faker.internet().emailAddress();
+        String address = faker.address().fullAddress();
+        System.out.println(name + " " + email +" " +address);
+        }
+}
